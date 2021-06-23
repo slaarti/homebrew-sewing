a8871b79f45bf678eb93c31a77731988908af54c
Chris Pinard <slarti@gweep.net>
2021-06-23 13:57:30 -0400

    Update Valentina to 0.7.47

--- Casks/valentina.rb
+++ Casks/valentina.rb
@@ -1,13 +1,12 @@
 cask "valentina" do
-  version "0.6.1"
-  sha256 "efdf35f741728bfda83615e9d866e5e8b5b01d702a6c9c1e512376b115ff4aa2"
+  version "0.7.47"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
-  url "https://bitbucket.org/valentinaproject/valentinaproject.bitbucket.io/downloads/valentina_#{version}_64bit-OSX.dmg",
+  url "https://bitbucket.org/valentinaproject/valentinaproject.bitbucket.io/downloads/valentina_#{version}_10.13_64bit-OSX.dmg",
       verified: "bitbucket.org/valentinaproject/valentinaproject.bitbucket.io/"
-  appcast "https://bitbucket.org/valentinaproject/valentinaproject.bitbucket.io/raw/ed35777f87ece53863569e80cf55f093548c7e3f/Appcast.xml"
   name "Valentina"
   desc "Open Source sewing pattern drafting software"
-  homepage "https://valentinaproject.bitbucket.io/"
+  homepage "https://smart-pattern.com.ua/en/valentina/download/"
 
   depends_on macos: ">= :el_capitan"
 
